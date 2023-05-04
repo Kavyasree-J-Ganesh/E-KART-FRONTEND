e8dbba8921275355a6b91332835412682bfb00d9
Shashank Rathore <shashank.rathore@facctum.com>
2023-05-04 10:22:28 +0530

    Address- create new and get all address

---
+++ src/Services/AddressService.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+
+const headerConfig = { headers: { Authorization: `bearer ${localStorage.getItem("auth")}` } }
+
+
+
+export const getAddress = async () => {
+    const result = await axios.get("http://localhost:6060/api/v1/address", headerConfig)
+    return result
+}
+
+
+export const deleteAddress = async (id) => {
+    const result = await axios.put(`http://localhost:6060/api/v1/address/delete/${id}`, {}, headerConfig)
+    return result
+}
+
+
