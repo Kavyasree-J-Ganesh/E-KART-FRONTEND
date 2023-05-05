693b36cb44b46287a034897fa49a6670603f9edd
Shashank Rathore <shashank.rathore@facctum.com>
2023-05-05 10:32:09 +0530

    Update: particular address for user

--- src/pages/Cart/AddressDetails.js
+++ src/pages/Cart/AddressDetails.js
@@ -6,196 +6,216 @@ import "./AddressDetails.css";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { useEffect } from "react";
-import CheckoutForm from "./Checkoutform"
-import { createAddress } from "../../Services/AddressService";
-
-
+import CheckoutForm from "./Checkoutform";
+// import { createAddress } from "../../Services/AddressService";
+import { createAddress } from "./../../Services/AddressService";
+import { useLocation } from "react-router-dom";
 
 function AddressDetails() {
-    const [fullName, setFullName] = useState("");
-    const [fullNameError, setFullNameError] = useState(false);
+  const [fullName, setFullName] = useState("");
+  const [fullNameError, setFullNameError] = useState(false);
 
-    const [mobileNumber, setMobileNumber] = useState("");
-    const [mobileNumberError, setMobileNumberError] = useState(false);
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [mobileNumberError, setMobileNumberError] = useState(false);
 
-    const [address, setAddress] = useState("");
-    const [addressError, setAddressError] = useState(false);
+  const [address, setAddress] = useState("");
+  const [addressError, setAddressError] = useState(false);
 
-    const [town, setTown] = useState("");
-    const [townError, setTownError] = useState(false);
+  const [town, setTown] = useState("");
+  const [townError, setTownError] = useState(false);
 
-    const [state, setState] = useState("");
-    const [stateError, setStateError] = useState(false);
+  const [state, setState] = useState("");
+  const [stateError, setStateError] = useState(false);
 
-    const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-    const [demoProduct] = useState({
-        name: "Sample",
-        price: 100,
-        description: "lorem Ipsum",
-    });
+  const location = useLocation();
 
-    const [clientSecret, setClientSecret] = useState("");
+  const [demoProduct] = useState({
+    name: "Sample",
+    price: 100,
+    description: "lorem Ipsum",
+  });
 
-    const validateFields = () => {
-        let isValid = true;
+  const [clientSecret, setClientSecret] = useState("");
 
-        if (fullName.trim() === "") {
-            setFullNameError(true);
-            isValid = false;
-        } else {
-            setFullNameError(false);
-        }
+  const validateFields = () => {
+    let isValid = true;
 
-        if (mobileNumber.trim() === "" || mobileNumber.trim().length !== 10) {
-            setMobileNumberError(true);
-            isValid = false;
-        } else {
-            setMobileNumberError(false);
-        }
+    if (fullName.trim() === "") {
+      setFullNameError(true);
+      isValid = false;
+    } else {
+      setFullNameError(false);
+    }
 
-        if (address.trim() === "") {
-            setAddressError(true);
-            isValid = false;
-        } else {
-            setAddressError(false);
-        }
+    if (mobileNumber.trim() === "" || mobileNumber.trim().length !== 10) {
+      setMobileNumberError(true);
+      isValid = false;
+    } else {
+      setMobileNumberError(false);
+    }
 
-        if (town.trim() === "") {
-            setTownError(true);
-            isValid = false;
-        } else {
-            setTownError(false);
-        }
+    if (address.trim() === "") {
+      setAddressError(true);
+      isValid = false;
+    } else {
+      setAddressError(false);
+    }
 
-        if (state.trim() === "") {
-            setStateError(true);
-            isValid = false;
-        } else {
-            setStateError(false);
-        }
+    if (town.trim() === "") {
+      setTownError(true);
+      isValid = false;
+    } else {
+      setTownError(false);
+    }
 
-        return isValid;
+    if (state.trim() === "") {
+      setStateError(true);
+      isValid = false;
+    } else {
+      setStateError(false);
+    }
+
+    return isValid;
+  };
+
+  useEffect(() => {
+    if (location.state?.isSubmitted) {
+      setIsSubmitted(true);
+    }
+    axios
+      .post(
+        "http://localhost:6060/api/v1/payment/createPaymentIntent",
+        {},
+        {
+          headers: {
+            Authorization: `bearer ${localStorage.getItem("auth")}`,
+          },
+        },
+      )
+      .then((res) => {
+        setClientSecret(res.data.charge.clientSecret);
+      })
+      .catch((err) => console.error("error in useEffect", err));
+  }, []);
+
+  const stripePromise = loadStripe(
+    "pk_test_51N12BsSHL3ZIWrpnuCIcbFzlmfLfaiEHDVYHVQdtCGuZcvHFgXBcnDnUyGMzifalYZ9rS3dtbWhq0OBoNGc0ZjBc00LPxcU5Hr",
+  );
+
+  const handleSubmit = () => {
+    let data = {
+      fullName,
+      mobile: mobileNumber,
+      address,
+      town,
+      state,
     };
+    createAddress(data)
+      .then((res) => {
+        console.log("address added", res.data);
+      })
+      .catch((err) => console.log("address Not added", err));
+    setIsSubmitted(true);
+  };
 
-    useEffect(() => {
-        axios
-            .post(
-                "http://localhost:6060/api/v1/payment/createPaymentIntent",
-                {},
-                {
-                    headers: {
-                        Authorization: `bearer ${localStorage.getItem("auth")}`,
-                    },
-                },
-            )
-            .then((res) => {
-                console.log("useEffect response", res.data);
-                setClientSecret(res.data.charge.clientSecret);
-            })
-            .catch((err) => console.error("error in useEffect", err));
-    }, []);
-
-    const stripePromise = loadStripe(
-        "pk_test_51N12BsSHL3ZIWrpnuCIcbFzlmfLfaiEHDVYHVQdtCGuZcvHFgXBcnDnUyGMzifalYZ9rS3dtbWhq0OBoNGc0ZjBc00LPxcU5Hr",
-    );
-
-    const handleSubmit = () => {
-        setIsSubmitted(true);
-    };
-
-    return (
-        <div>
-            {stripePromise && clientSecret ? (
-                <Elements
-                    stripe={stripePromise}
-                    options={{ clientSecret: clientSecret }}
-                >
-                    <div className="address-details-container">
-                        {isSubmitted ? (
-                            <CheckoutForm />
-                        ) : (
-                            <>
-                                <h2>Enter your address details</h2>
-                                <form>
-                                    <TextField
-                                        label="Full Name"
-                                        value={fullName}
-                                        onChange={(e) => setFullName(e.target.value)}
-                                        error={fullNameError}
-                                        helperText={
-                                            fullNameError ? "Please enter your full name" : ""
-                                        }
-                                        required
-                                    />
-                                    <br />
-                                    <TextField
-                                        label="Mobile Number"
-                                        value={mobileNumber}
-                                        onChange={(e) => setMobileNumber(e.target.value)}
-                                        error={mobileNumberError}
-                                        helperText={
-                                            mobileNumberError
-                                                ? "Please enter a valid mobile number"
-                                                : ""
-                                        }
-                                        required
-                                    />
-                                    <br />
-                                    <TextField
-                                        label="Address"
-                                        value={address}
-                                        onChange={(e) => setAddress(e.target.value)}
-                                        error={addressError}
-                                        helperText={addressError ? "Please enter your address" : ""}
-                                        required
-                                    />
-                                    <br />
-                                    <TextField
-                                        label="Town"
-                                        value={town}
-                                        onChange={(e) => setTown(e.target.value)}
-                                        error={townError}
-                                        helperText={townError ? "Please enter your town" : ""}
-                                        required
-                                    />
-                                    <br />
-                                    <TextField
-                                        label="State"
-                                        value={state}
-                                        onChange={(e) => setState(e.target.value)}
-                                        error={stateError}
-                                        helperText={stateError ? "Please enter your state" : ""}
-                                        required
-                                    />
-                                    <br />
-                                    <div className="checkout-button-container">
-                                        <button className="checkout_button" onClick={handleSubmit
-                                            // , createAddress(product.id)
-                                            //     .then((res) => {
-                                            //         setUpdateData(!updateData);
-                                            //     })
-                                            //     .catch((err) => {
-                                            //         console.error("error :", err);
-                                            //         setUpdateData(!updateData);
-                                            //     });
-                                        } type="button">
-                                            Submit
-                                        </button>
-                                        {/* </StripeCheckout> */}
-                                    </div>
-                                </form>
-                            </>
-                        )}
-                    </div>
-                </Elements>
+  return (
+    <div>
+      {stripePromise && clientSecret ? (
+        <Elements
+          stripe={stripePromise}
+          options={{ clientSecret: clientSecret }}
+        >
+          <div className="address-details-container">
+            {isSubmitted ? (
+              <CheckoutForm />
             ) : (
-                ""
-            )
-            }
-            {/* <Header /> */}
-        </div >
-    );
+              <>
+                <h2>Enter your address details</h2>
+                <form>
+                  <TextField
+                    label="Full Name"
+                    value={fullName}
+                    onChange={(e) => setFullName(e.target.value)}
+                    error={fullNameError}
+                    helperText={
+                      fullNameError ? "Please enter your full name" : ""
+                    }
+                    required
+                  />
+                  <br />
+                  <TextField
+                    label="Mobile Number"
+                    value={mobileNumber}
+                    onChange={(e) => setMobileNumber(e.target.value)}
+                    error={mobileNumberError}
+                    helperText={
+                      mobileNumberError
+                        ? "Please enter a valid mobile number"
+                        : ""
+                    }
+                    required
+                  />
+                  <br />
+                  <TextField
+                    label="Address"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    error={addressError}
+                    helperText={addressError ? "Please enter your address" : ""}
+                    required
+                  />
+                  <br />
+                  <TextField
+                    label="Town"
+                    value={town}
+                    onChange={(e) => setTown(e.target.value)}
+                    error={townError}
+                    helperText={townError ? "Please enter your town" : ""}
+                    required
+                  />
+                  <br />
+                  <TextField
+                    label="State"
+                    value={state}
+                    onChange={(e) => setState(e.target.value)}
+                    error={stateError}
+                    helperText={stateError ? "Please enter your state" : ""}
+                    required
+                  />
+                  <br />
+                  <div className="checkout-button-container">
+                    <button
+                      className="checkout_button"
+                      onClick={
+                        handleSubmit
+                        // , createAddress(product.id)
+                        //     .then((res) => {
+                        //         setUpdateData(!updateData);
+                        //     })
+                        //     .catch((err) => {
+                        //         console.error("error :", err);
+                        //         setUpdateData(!updateData);
+                        //     });
+                      }
+                      type="button"
+                    >
+                      Submit
+                    </button>
+                    {/* </StripeCheckout> */}
+                  </div>
+                </form>
+              </>
+            )}
+          </div>
+        </Elements>
+      ) : (
+        ""
+      )}
+      {/* <Header /> */}
+    </div>
+  );
 }
 
 export default AddressDetails;
