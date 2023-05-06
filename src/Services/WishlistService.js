9db64e42d0a31afb9623d18683b2070eaa693f67
Shashank Rathore <shashank.rathore@facctum.com>
2023-05-06 14:26:29 +0530

    particular wishlist and address for user

--- src/Services/WishlistService.js
+++ src/Services/WishlistService.js
@@ -4,6 +4,10 @@ const headerConfig = {
   headers: { Authorization: `bearer ${localStorage.getItem("auth")}` },
 };
 
+const getToken = () => {
+  return localStorage.getItem("auth");
+};
+
 export const addToWishlist = async (id) => {
   const result = await axios.post(
     `http://localhost:6060/api/v1/wishlist/${id}`,
@@ -14,9 +18,11 @@ export const addToWishlist = async (id) => {
 };
 
 export const getWishlist = async () => {
+  const token = getToken();
   const result = await axios.get(
-    `http://localhost:6060/api/v1/wishlist/`,
-    headerConfig,
+    `http://localhost:6060/api/v1/wishlist/`, {
+    headers: { Authorization: `bearer ${token}` },
+  }
   );
   return result;
 };
